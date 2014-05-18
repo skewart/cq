34e820444c7ad492ea21f3d75534c18008433f7f
Scott Ewart <scott.ewart@aditazz.com>
2014-05-17 17:56:54 -0700

    Argh... forgot to get rid of obsolete reference to environment variable.

--- cq.py
+++ cq.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-API_KEY = os.environ['API_KEY']
+#API_KEY = os.environ['API_KEY']
 
 @app.route('/')
 def hello_world():
-	return render_template('hello_maps.html', api_key=API_KEY )
+	return render_template('hello_maps.html' )
    
 
 if __name__ == '__main__':
